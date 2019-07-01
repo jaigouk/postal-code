30476722c4323ce982beb6e2336c4070a785e792
Jaigouk Kim <ping@jaigouk.kim>
2019-07-01 03:37:11 +0200

    Added PostalCode::Tile38.nearby

--- lib/postal_code/tile38.rb
+++ lib/postal_code/tile38.rb
@@ -39,14 +39,44 @@ module PostalCode
         { error: e.message }
       end
 
+      # geojson part is json string.
+      # For a given hash like this,
+      # hash = {type: 'Point', coordinates: [134.9583232, 34.6658157]}
+      # hash.to_json should be the argument for the geojson
+      #
+      # https://github.com/tidwall/tile38#geojson
+      # mportant to note that all coordinates are in Longitude, Latitude order.
+      # If it's a valid geojson, then it's ok to use.
       def insert_geojson(namespace:, key:, geojson:)
         set(namespace, key, 'OBJECT', geojson)
+      end
+
+      # range 20000 m by default.
+      # results are sorted for nearest ones first by default.
+      #
+      # Usage:
+      #   PostalCode::Tile38.nearby(key: 'postalcode',
+      #                             lat: 34.6658157, lon: 134.9583232)
+      def nearby(key:, lat:, lon:, range: '2000')
+        cmd = [:nearby, key, 'LIMIT', 5, 'POINT', lat, lon, range]
+        res = redis.call(cmd)[1]
+        first_result = res.blank? ? [] : res.first[1]
+        parse_result(first_result)
+      end
+
+      def scan(key)
+        redis.call([:scan, key])
+      end
+
+      def country_for(lat:, lon:)
+        res = redis.call([:nearby, 'country', 'LIMIT', 1, 'POINT', lat, lon])[1]
+        parse_result(res.first[1])
       end
 
       private
 
       def parse_result(res)
-        return { error: 'not_found' } if res.nil?
+        return { error: 'not_found' } if res.blank?
         return { success: 'set the data' } if res == 'OK'
 
         Oj.load(res)
--- spec/postal_code/tile38_spec.rb
+++ spec/postal_code/tile38_spec.rb
@@ -8,9 +8,14 @@ describe PostalCode::Tile38 do
     PostalCode::Tile38.redis.del('fleet', 'bus1')
     PostalCode::Tile38.redis.del('fleet', 'bus2')
     PostalCode::Tile38.redis.del('fleet', 'bus3')
-    PostalCode::Tile38.redis.del('japan', '538425831')
-    PostalCode::Tile38.redis.del('japan', '85672871')
-    PostalCode::Tile38.redis.del('japan', '85911073')
+    PostalCode::Tile38.redis.del('postalcode', '538425831')
+    PostalCode::Tile38.redis.del('region', '85672871')
+    PostalCode::Tile38.redis.del('neighbourhood', '85911073')
+    PostalCode::Tile38.redis.del('region', '85672769')
+    PostalCode::Tile38.redis.del('county', '890524505')
+    PostalCode::Tile38.redis.del('locality', '1344243079')
+    PostalCode::Tile38.redis.del('neighbourhood', '85935557')
+    PostalCode::Tile38.redis.del('neighbourhood', '1126075403')
   end
 
   describe '#redis' do
@@ -74,8 +79,8 @@ describe PostalCode::Tile38 do
 
     context 'point' do
       it 'inserts point geojson and properties' do
-        PostalCode::Tile38.insert_geojson(namespace: 'japan', key: '538425831', geojson: point)
-        got = PostalCode::Tile38.get('japan', '538425831')
+        PostalCode::Tile38.insert_geojson(namespace: 'postalcode', key: '538425831', geojson: point)
+        got = PostalCode::Tile38.get('postalcode', '538425831')
         hierarchy =[{"continent_id" => "102191569",
                      "country_id" => "85632429",
                      "locality_id" => 102031667,
@@ -92,8 +97,8 @@ describe PostalCode::Tile38 do
 
     context 'polygon' do
       it 'inserts polygon geojson and properties' do
-        PostalCode::Tile38.insert_geojson(namespace: 'japan', key: '85672871', geojson: polygon)
-        got = PostalCode::Tile38.get('japan', '85672871')
+        PostalCode::Tile38.insert_geojson(namespace: 'region', key: '85672871', geojson: polygon)
+        got = PostalCode::Tile38.get('region', '85672871')
         hierarchy = [{"continent_id"=>102191569, "country_id"=>85632429, "region_id"=>85672871}]
 
         expect(got['type']).to eq('Feature')
@@ -106,8 +111,8 @@ describe PostalCode::Tile38 do
 
     context 'multi_polygon' do
       it 'inserts polygon geojson and properties' do
-        PostalCode::Tile38.insert_geojson(namespace: 'japan', key: '85911073', geojson: multi_polygon)
-        got = PostalCode::Tile38.get('japan', '85911073')
+        PostalCode::Tile38.insert_geojson(namespace: 'neighbourhood', key: '85911073', geojson: multi_polygon)
+        got = PostalCode::Tile38.get('neighbourhood', '85911073')
         hierarchy =  [{"continent_id"=>102191569,
                        "country_id"=>85632429,
                        "county_id"=>1108741697,
@@ -124,32 +129,57 @@ describe PostalCode::Tile38 do
     end
   end
 
-  # describe '#point_in_polygon' do
-  #   context 'polygon' do
-  #     it '' do
+  describe '#find_postal_code' do
+    let(:region) { File.read("#{PostalCode::ROOT_PATH}/spec/fixtures/1_region_85672769.geojson") }
+    let(:county) { File.read("#{PostalCode::ROOT_PATH}/spec/fixtures/2_county_890524505.geojson") }
+    let(:locality) { File.read("#{PostalCode::ROOT_PATH}/spec/fixtures/3_locality_1344243079.geojson") }
+    let(:neighbourhood1) { File.read("#{PostalCode::ROOT_PATH}/spec/fixtures/4_neighbourhood_85935557.geojson") }
+    let(:neighbourhood2) { File.read("#{PostalCode::ROOT_PATH}/spec/fixtures/5_neighbourhood_1126075403.geojson") }
 
-  #     end
-  #   end
+    before do
+      PostalCode::Tile38.insert_geojson(namespace: 'region', key: '85672769', geojson: region)
+      PostalCode::Tile38.insert_geojson(namespace: 'county', key: '890524505', geojson: county)
+      PostalCode::Tile38.insert_geojson(namespace: 'locality', key: '1344243079', geojson: locality)
+      PostalCode::Tile38.insert_geojson(namespace: 'neighbourhood', key: '85935557', geojson: neighbourhood1)
+      PostalCode::Tile38.insert_geojson(namespace: 'neighbourhood', key: '1126075403', geojson: neighbourhood2)
+    end
 
-  #   context 'multi_polygon' do
-  #     it '' do
+    context('locality') do
+      it 'returns not found if there is no matching' do
+        got = PostalCode::Tile38.nearby(key: 'region', lat: 34.6658157, lon: 134.9583232)
+        expect(got).to eq({:error=>"not_found"})
+      end
 
-  #     end
-  #   end
-  # end
+      it 'searches namespace with coordinates.' do
+        # 2-chōme-6-2 西明石西町１丁目 Akashi-shi, Hyōgo-ken 673-0049, Japan
+        # 〒673-0049 兵庫県明石市西明石西町１丁目２丁目６−2
+        # https://www.google.com/maps/place/%E3%81%A1%E3%82%8D%E3%82%8A%E3%82%93%E6%9D%91/@34.6658157,134.9583232,20.89z/data=!4m13!1m7!3m6!1s0x0:0x0!2zMzTCsDM5JzU3LjAiTiAxMzTCsDU3JzMwLjIiRQ!3b1!8m2!3d34.6658198!4d134.9583807!3m4!1s0x3554d46085531783:0x770e780916a7cc16!8m2!3d34.6658807!4d134.9583185
+        # lat,lon = 34.6658157,134.9583232
+        # name = ちろりん村 (a pet store)
 
-  # describe '#polyons_for_point' do
-  #   context 'polygon' do
-  #     it '' do
+        # 2 results. 2 km range
+        # PostalCode::Tile38.redis.call([:nearby, 'neighbourhood', 'POINT', "34.6658157", "134.9583232", "2000"])
 
-  #     end
-  #   end
+        # From 1126075403 to the petstore takes 17min. 1.3km walking distance
+        # https://www.google.ae/maps/dir/'34.6658157,134.9583232'/34.674088,134.951472/@34.6699524,134.9503895,16z/data=!3m1!4b1!4m7!4m6!1m3!2m2!1d134.9583232!2d34.6658157!1m0!3e2
 
-  #   context 'multi_polygon' do
-  #     it '' do
+        # Frome 85935557 to the petstore 1.3km. 16min walking distance.
+        # https://www.google.ae/maps/dir/'34.666196,134.958252'/34.674088,134.951472/@34.6701418,134.9503895,16z/data=!3m1!4b1!4m7!4m6!1m3!2m2!1d134.958252!2d34.666196!1m0!3e2
+        # 1.3
+        got = PostalCode::Tile38.nearby(key: 'neighbourhood', lat: 34.6658157, lon: 134.9583232)
+        hierarchy = [{"continent_id"=>102191569,
+                      "country_id"=>85632429,
+                      "county_id"=>890524505,
+                      "locality_id"=>-1,
+                      "neighbourhood_id"=>85935557,
+                      "region_id"=>85672769}]
 
-  #     end
-  #   end
-  # end
-
+        expect(got['type']).to eq('Feature')
+        expect(got['properties']['wof:hierarchy']).to eq(hierarchy)
+        expect(got['id']).to eq(85935557)
+        expect(got['geometry']['type']).to eq('Point')
+        expect(got['geometry']['coordinates'].size).to eq 2
+      end
+    end
+  end
 end
